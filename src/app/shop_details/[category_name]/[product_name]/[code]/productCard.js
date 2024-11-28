2ceb67e4158f7f908e3f963b929ba9b6cea8b96f
alex <eafo72@gmail.com>
2024-11-28 11:34:26 -0600

    btn mercado libre

--- src/app/shop_details/[category_name]/[product_name]/[code]/productCard.js
+++ src/app/shop_details/[category_name]/[product_name]/[code]/productCard.js
@@ -4,45 +4,47 @@ import { useState } from "react";
 
 import Link from "next/link";
 
-import Modal from 'react-modal';
+import Modal from "react-modal";
 
 import Select from "react-select";
 
-import { useCartStore } from '../../../../../store/cart';
+import { useCartStore } from "../../../../../store/cart";
 
-import { toast } from 'sonner';
+import { toast } from "sonner";
 
 import ImageGallery from "react-image-gallery";
 
 import { FacebookShareButton, FacebookIcon } from "react-share";
 
-export const ProductCard = ({ stockTotal, fotos_carrusel, producto, allSizes }) => {
-
-  const addCartItem = useCartStore((state) => state.add_cart_item)
-  const { cart } = useCartStore()
+export const ProductCard = ({
+  stockTotal,
+  fotos_carrusel,
+  producto,
+  allSizes,
+}) => {
+  const addCartItem = useCartStore((state) => state.add_cart_item);
+  const { cart } = useCartStore();
 
   const [showContinueModal, setContinueModal] = useState(false);
   const [talla, setTalla] = useState();
   const [color, setColor] = useState();
   const [cantidad, setCantidad] = useState(1);
 
-  let images = [{
-    original: producto.foto_principal,
-    thumbnail: producto.foto_principal,
-    originalClass: "img-fluid",
-  }]; 
- 
-  for (let i = 0; i < fotos_carrusel.length; i++) {
-      images.push(
-      {
-        original: fotos_carrusel[i]["image"],
-        thumbnail: fotos_carrusel[i]["image"],
-        originalClass: "img-fluid",
-      },
-      );
-  }
+  let images = [
+    {
+      original: producto.foto_principal,
+      thumbnail: producto.foto_principal,
+      originalClass: "img-fluid",
+    },
+  ];
 
-  
+  for (let i = 0; i < fotos_carrusel.length; i++) {
+    images.push({
+      original: fotos_carrusel[i]["image"],
+      thumbnail: fotos_carrusel[i]["image"],
+      originalClass: "img-fluid",
+    });
+  }
 
   const currentPageUrl =
     "https://flagasamascotas.com/shop_details/" +
@@ -65,9 +67,8 @@ export const ProductCard = ({ stockTotal, fotos_carrusel, producto, allSizes }) 
   };
 
   const addToCart = (event) => {
-
     event.preventDefault();
-/*
+    /*
     let onCart = 0;
 
     //validamos campos
@@ -124,13 +125,13 @@ export const ProductCard = ({ stockTotal, fotos_carrusel, producto, allSizes }) 
 
   const modalCustomStyles = {
     content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-      animation: 'fadeIn 1s'
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      animation: "fadeIn 1s",
     },
   };
 
@@ -138,15 +139,15 @@ export const ProductCard = ({ stockTotal, fotos_carrusel, producto, allSizes }) 
     control: (base, state) => ({
       ...base,
       background: "white",
-      textTransform:"none",
-      paddingLeft:"10px",
-      borderColor: state.isFocused ? "#25BC6C":"#cccccc",
+      textTransform: "none",
+      paddingLeft: "10px",
+      borderColor: state.isFocused ? "#25BC6C" : "#cccccc",
       boxShadow: "0",
       "&:hover": {
         ...base,
         boxShadow: "0",
         borderColor: "#25BC6C !important",
-      }
+      },
     }),
     singleValue: (base, state) => ({
       ...base,
@@ -159,7 +160,11 @@ export const ProductCard = ({ stockTotal, fotos_carrusel, producto, allSizes }) 
     option: (base, state) => {
       return {
         ...base,
-        background: state.isSelected ? "#25BC6C" : state.isFocused ? "#25BC6C50": "transparent",
+        background: state.isSelected
+          ? "#25BC6C"
+          : state.isFocused
+          ? "#25BC6C50"
+          : "transparent",
         color: state.isSelected ? "white" : "grey",
       };
     },
@@ -167,39 +172,55 @@ export const ProductCard = ({ stockTotal, fotos_carrusel, producto, allSizes }) 
 
   return (
     <>
-    <section className="shop_details bg_gray sec_space_small" style={{marginTop:"30px"}}>
-      <div className="container">
-        <div className="row ">
-          <div className="col col-lg-7 col-md-8">
-            <div style={{paddingTop:"20px"}}>
-              <ImageGallery items={images} />
-            </div> 
-          </div>
+      <section
+        className="shop_details bg_gray sec_space_small"
+        style={{ marginTop: "30px" }}
+      >
+        <div className="container">
+          <div className="row ">
+            <div className="col col-lg-7 col-md-8">
+              <div style={{ paddingTop: "20px" }}>
+                <ImageGallery items={images} />
+              </div>
+            </div>
 
-          <div className="col col-lg-5 col-md-8">
-            <div className="shop_details_content" style={{paddingBottom:"20px"}}>
-              {/*
+            <div className="col col-lg-5 col-md-8">
+              <div
+                className="shop_details_content"
+                style={{ paddingBottom: "20px" }}
+              >
+                {/*
                 <div className="item_badge">Sale</div>
                 */}
-              <h2 className="item_subtitle">
-                {producto && producto.categoria}
-              </h2>
-              <h3 className="item_title" style={{fontFamily:"Plantagenet"}}>{producto && producto.nombre}</h3>
-              <h4>{producto && producto.marca}</h4>
-              <div className="item_price">
-                <span className="sale_price">
-                  {/*
+                <h2 className="item_subtitle">
+                  {producto && producto.categoria}
+                </h2>
+                <h3
+                  className="item_title"
+                  style={{ fontFamily: "Plantagenet" }}
+                >
+                  {producto && producto.nombre}
+                </h3>
+                <h4>{producto && producto.marca}</h4>
+                <div className="item_price">
+                  <span className="sale_price">
+                    {/*
                   $ {producto && producto.precio}
                   */}
-                </span>
-                {/*
+                  </span>
+                  {/*
                   <del>$130.00</del>
                    */}
-              </div>
-              <p className="mb-0">{producto && producto.descripcion}</p>
-              <hr />
+                </div>
+                <p className="mb-0">{producto && producto.descripcion}</p>
+                <hr />
+                <a href="https://listado.mercadolibre.com.mx/_CustId_613207285?item_id=MLM797610603&category_id=MLM1077&seller_id=613207285&client=recoview-selleritems&recos_listing=true#origin=vip&component=sellerData&typeSeller=classic" style="text-decoration: none;">
+                  <div style="background-color: #ffe600; color: #000; padding: 10px 20px; border-radius: 5px; font-size: 16px; font-weight: bold; text-align: center; width: fit-content;">
+                    Comprar en Mercado Libre
+                  </div>
+                </a>
 
-              {/*
+                {/*
               <div className="sd_info_layout">
                 <h4 className="title_text">Color:</h4>
 
@@ -236,7 +257,7 @@ export const ProductCard = ({ stockTotal, fotos_carrusel, producto, allSizes }) 
               </div>
               */}
 
-              {/*
+                {/*
               <ul className="sd_btns_group ul_li">
                 <li>
                   <div className="quantity_form">
@@ -273,46 +294,61 @@ export const ProductCard = ({ stockTotal, fotos_carrusel, producto, allSizes }) 
                 </li>
               </ul>
               */}
-              <hr />
+                <hr />
 
-              <div className="row align-items-center justify-content-between">
-                <div className="col col-lg-7 col-md-6">
-                  <div className="sd_info_layout mb-0">
-                    <h4 className="title_text">Compartir:</h4>
-                    <ul className="social_primary ul_li">
-                      <li>
-                          <FacebookShareButton
-                          url={currentPageUrl}
-                          >
+                <div className="row align-items-center justify-content-between">
+                  <div className="col col-lg-7 col-md-6">
+                    <div className="sd_info_layout mb-0">
+                      <h4 className="title_text">Compartir:</h4>
+                      <ul className="social_primary ul_li">
+                        <li>
+                          <FacebookShareButton url={currentPageUrl}>
                             <i className="fab fa-facebook-f"></i>
-                            
                           </FacebookShareButton>
-                      </li>
-                    </ul>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-    <Modal
+      </section>
+      <Modal
         isOpen={showContinueModal}
         style={modalCustomStyles}
         ariaHideApp={false}
       >
-        <ul style={{listStyle:"none",display:"flex",padding:"0px",margin:"0px"}}>
+        <ul
+          style={{
+            listStyle: "none",
+            display: "flex",
+            padding: "0px",
+            margin: "0px",
+          }}
+        >
           <li>
-            <button className="btn btn_primary btn_rounded text-uppercase" style={{padding:"15px",margin:"10px"}} onClick={closeModal}>Continuar comprando</button>
+            <button
+              className="btn btn_primary btn_rounded text-uppercase"
+              style={{ padding: "15px", margin: "10px" }}
+              onClick={closeModal}
+            >
+              Continuar comprando
+            </button>
           </li>
           <li>
-            <Link href="/cart"><button className="btn btn_secondary btn_rounded text-uppercase" style={{padding:"15px",margin:"10px"}}>Ir al carrito</button></Link>
+            <Link href="/cart">
+              <button
+                className="btn btn_secondary btn_rounded text-uppercase"
+                style={{ padding: "15px", margin: "10px" }}
+              >
+                Ir al carrito
+              </button>
+            </Link>
           </li>
         </ul>
       </Modal>
     </>
   );
 };
-
-
