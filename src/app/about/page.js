626baaf0c99cb9d3ddf4d052ba4f05e2d8620b8b
alex <eafo72@gmail.com>
2025-01-10 17:16:33 -0600

    ABOUT SECTION

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import Image from 'next/image'
-import about_image from "../images/about/about-vision.jpg";
+import Image from 'next/image';
+import about_image from "../../images/about/about-vision.jpg";
 
 
 const About = async () => {
