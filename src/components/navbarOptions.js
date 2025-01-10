7f69b119d47fc950df1e5eb1f6fff23b633a5f1e
alex <eafo72@gmail.com>
2025-01-10 16:46:49 -0600

    NEW SECTIONS

--- src/components/navbarOptions.js
+++ src/components/navbarOptions.js
@@ -14,142 +14,197 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 export const NavbarOptions = ({ categories }) => {
   const { cart } = useCartStore(); //debe de ir aqui arriba por ser un hook y evitar errores
 
-  
   const pathname = usePathname();
 
   function openNav() {
     document.getElementById("mySidenav").style.width = "70%";
   }
-  
+
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
 
-  
   return (
     <>
-    <div className="menu_wrap" key={pathname}>
+      <div className="menu_wrap" key={pathname}>
         <nav className="main_menu navbar navbar-expand-lg">
-        <button
-          id="btn_hamburger"
-          onClick={() => {openNav()}} 
-          className="mobile_menu_btn navbar-toggler"
-          type="button"
-        >
-          <span className="navbar-toggler-icon">
-            <i className="fal fa-bars"></i>
-          </span>
-        </button>
+          <button
+            id="btn_hamburger"
+            onClick={() => {
+              openNav();
+            }}
+            className="mobile_menu_btn navbar-toggler"
+            type="button"
+          >
+            <span className="navbar-toggler-icon">
+              <i className="fal fa-bars"></i>
+            </span>
+          </button>
 
-        <motion.div
-        initial={{y:-20, opacity:0}}
-        animate={{y:0, opacity:1}}
-        
-        transition={{
-          ease: "easeInOut",
-          duration: 0.75
-        }}       
-          className="main_menu_inner collapse navbar-collapse"
-          id="main_menu_dropdown"
-        >
-
-          <ul className="main_menu_list ul_li">
-
-            {pathname.startsWith("/categories") ? (
-              <li className="active dropdown">
-                  <a className="nav-link" href="#" id="categories_submenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Fórmulas especializadas
+          <motion.div
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              ease: "easeInOut",
+              duration: 0.75,
+            }}
+            className="main_menu_inner collapse navbar-collapse"
+            id="main_menu_dropdown"
+          >
+            <ul className="main_menu_list ul_li">
+              {pathname.startsWith("/categories") ? (
+                <li className="active dropdown">
+                  <a
+                    className="nav-link"
+                    href="#"
+                    id="categories_submenu"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Fórmulas especializadas
                   </a>
-                  <div className="megamenu dropdown-menu" aria-labelledby="categories_submenu">
+                  <div
+                    className="megamenu dropdown-menu"
+                    aria-labelledby="categories_submenu"
+                  >
                     <div className="container">
-                        <ul className="homepage_list ul_li">
-                          {categories && categories.map((item, index) => (
+                      <ul className="homepage_list ul_li">
+                        {categories &&
+                          categories.map((item, index) => (
                             <li key={index}>
-                              <a href={`/categories/Todas/${item.nombreSlugged}`}>
-                                <span className="item_image" style={{backgroundColor:"transparent"}}>
-                                  <img src={item.imagen} style={{height:"60px"}} alt={item.imagen} className="img-fluid"/>
+                              <a
+                                href={`/categories/Todas/${item.nombreSlugged}`}
+                              >
+                                <span
+                                  className="item_image"
+                                  style={{ backgroundColor: "transparent" }}
+                                >
+                                  <img
+                                    src={item.imagen}
+                                    style={{ height: "60px" }}
+                                    alt={item.imagen}
+                                    className="img-fluid"
+                                  />
                                 </span>
-                                <span className="item_title">{item.nombre}</span>
+                                <span className="item_title">
+                                  {item.nombre}
+                                </span>
                               </a>
                             </li>
                           ))}
-                        </ul>
+                      </ul>
                     </div>
                   </div>
-              </li>
-            ) : (
-              <li className="dropdown">
-                  <a className="nav-link" href="#" id="categories_submenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Fórmulas especializadas
+                </li>
+              ) : (
+                <li className="dropdown">
+                  <a
+                    className="nav-link"
+                    href="#"
+                    id="categories_submenu"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Fórmulas especializadas
                   </a>
-                  <div className="megamenu dropdown-menu" aria-labelledby="categories_submenu">
+                  <div
+                    className="megamenu dropdown-menu"
+                    aria-labelledby="categories_submenu"
+                  >
                     <div className="container">
-                        <ul className="homepage_list ul_li">
-                          {categories && categories.map((item, index) => (
+                      <ul className="homepage_list ul_li">
+                        {categories &&
+                          categories.map((item, index) => (
                             <li key={index}>
-                              <a href={`/categories/Todas/${item.nombreSlugged}`}>
-                                <span className="item_image" style={{backgroundColor:"transparent"}}>
-                                  <img src={item.imagen} style={{height:"60px"}} alt={item.imagen} className="img-fluid"/>
+                              <a
+                                href={`/categories/Todas/${item.nombreSlugged}`}
+                              >
+                                <span
+                                  className="item_image"
+                                  style={{ backgroundColor: "transparent" }}
+                                >
+                                  <img
+                                    src={item.imagen}
+                                    style={{ height: "60px" }}
+                                    alt={item.imagen}
+                                    className="img-fluid"
+                                  />
                                 </span>
-                                <span className="item_title">{item.nombre}</span>
+                                <span className="item_title">
+                                  {item.nombre}
+                                </span>
                               </a>
                             </li>
                           ))}
-                        </ul>
+                      </ul>
                     </div>
                   </div>
-              </li>
-            )}
+                </li>
+              )}
 
-            {pathname === "/faq" ? (
-              <li className="active dropdown">
-                <Link
-                  className="nav-link"
-                  href="/faq"
-                  
-                >
-                  FAQ
-                </Link>
-              </li>
-            ) : (
-              <li className="dropdown">
-                <Link
-                  className="nav-link"
-                  href="/faq"
-                  
-                >
-                  FAQ
-                </Link>
-              </li>
-            )}
+              {pathname === "/about" ? (
+                <li className="active dropdown">
+                  <Link className="nav-link" href="/about">
+                    QUIÉNES SOMOS
+                  </Link>
+                </li>
+              ) : (
+                <li className="dropdown">
+                  <Link className="nav-link" href="/about">
+                    QUIÉNES SOMOS
+                  </Link>
+                </li>
+              )}
 
-            {pathname === "/contact" ? (
-              <li className="active dropdown">
-                <Link
-                  className="nav-link"
-                  href="/contact"
-                  
-                >
-                  Contacto
-                </Link>
-              </li>
-            ) : (
-              <li className="dropdown">
-                <Link
-                  className="nav-link"
-                  href="/contact"
-                  
-                >
-                  Contacto
-                </Link>
-              </li>
-            )}
-          </ul>
-        </motion.div>
-      </nav>
+              {pathname === "/resellers" ? (
+                <li className="active dropdown">
+                  <Link className="nav-link" href="/resellers">
+                    DISTRIBUIDORES
+                  </Link>
+                </li>
+              ) : (
+                <li className="dropdown">
+                  <Link className="nav-link" href="/resellers">
+                  DISTRIBUIDORES
+                  </Link>
+                </li>
+              )}
 
-      <ul className="header_icons_group ul_li icons_position_normal">
-        {/*
+              {pathname === "/faq" ? (
+                <li className="active dropdown">
+                  <Link className="nav-link" href="/faq">
+                    FAQ
+                  </Link>
+                </li>
+              ) : (
+                <li className="dropdown">
+                  <Link className="nav-link" href="/faq">
+                    FAQ
+                  </Link>
+                </li>
+              )}
+
+              {pathname === "/contact" ? (
+                <li className="active dropdown">
+                  <Link className="nav-link" href="/contact">
+                    Contacto
+                  </Link>
+                </li>
+              ) : (
+                <li className="dropdown">
+                  <Link className="nav-link" href="/contact">
+                    Contacto
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </motion.div>
+        </nav>
+
+        <ul className="header_icons_group ul_li icons_position_normal">
+          {/*
                 <li>
                   <button type="button" className="main_search_btn" data-bs-toggle="collapse" data-bs-target="#main_search_collapse" aria-expanded="false" aria-controls="main_search_collapse">
                     <i className="search_icon far fa-search"></i>
@@ -157,7 +212,7 @@ export const NavbarOptions = ({ categories }) => {
                   </button>
                 </li>
                 */}
-        {/*
+          {/*
         <li>
           <Link className="cart_btn" href="/cart" onClick={() => {closeNav()}} style={{marginRight:"20px"}}>
             <i className="far fa-shopping-bag"></i>
@@ -165,36 +220,93 @@ export const NavbarOptions = ({ categories }) => {
           </Link>
         </li>
         */}
-      </ul>
-    </div>
+        </ul>
+      </div>
 
-    <div id="mySidenav" className="sidenav">
+      <div id="mySidenav" className="sidenav">
+        <button
+          className="closebtn"
+          onClick={() => {
+            closeNav();
+          }}
+        >
+          <i className="fal fa-times"></i>
+        </button>
 
-      <button className="closebtn" onClick={() => {closeNav()}}  ><i className="fal fa-times"></i></button>
+        <Link
+          href="#menu_categorias"
+          data-bs-toggle="collapse"
+          aria-expanded="true"
+        >
+          Fórmulas especializadas
+        </Link>
 
-      <Link href="#menu_categorias" data-bs-toggle="collapse" aria-expanded="true">Fórmulas especializadas</Link>
-      
-      <div id="menu_categorias" className="collapse show">
-        {categories && categories.map((item, index) => (
-          
-            <Link href={`/categories/Todas/${item.nombreSlugged}`}  onClick={() => {closeNav()}} key={index} className="submenu" >
-              {item.nombre}
-              <FontAwesomeIcon
-                icon={faArrowRight}
-                style={{position:"absolute",right:"25px",color:"#25BC6C"}}
-              />
-            </Link>
-          
-        ))}
-      </div>              
+        <div id="menu_categorias" className="collapse show">
+          {categories &&
+            categories.map((item, index) => (
+              <Link
+                href={`/categories/Todas/${item.nombreSlugged}`}
+                onClick={() => {
+                  closeNav();
+                }}
+                key={index}
+                className="submenu"
+              >
+                {item.nombre}
+                <FontAwesomeIcon
+                  icon={faArrowRight}
+                  style={{
+                    position: "absolute",
+                    right: "25px",
+                    color: "#25BC6C",
+                  }}
+                />
+              </Link>
+            ))}
+        </div>
 
-      <Link href="/faq" onClick={() => {closeNav()}} className="submenu">FAQ</Link>
-      <Link href="/contact" onClick={() => {closeNav()}} className="submenu">Contacto</Link>
+        <Link
+          href="/about"
+          onClick={() => {
+            closeNav();
+          }}
+          className="submenu"
+        >
+          QUIÉNES SOMOS
+        </Link>
 
-      
+        <Link
+          href="/resellers"
+          onClick={() => {
+            closeNav();
+          }}
+          className="submenu"
+        >
+          DISTRIBUIDORES
+        </Link>
 
-    </div>
 
-  </>
+        <Link
+          href="/faq"
+          onClick={() => {
+            closeNav();
+          }}
+          className="submenu"
+        >
+          FAQ
+        </Link>
+
+        <Link
+          href="/contact"
+          onClick={() => {
+            closeNav();
+          }}
+          className="submenu"
+        >
+          Contacto
+        </Link>
+
+      </div>
+    </>
   );
 };
