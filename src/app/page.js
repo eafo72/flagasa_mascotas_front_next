ed67e6d857cf64cbaae6e782fa710a4eeb066f3f
alex <eafo72@gmail.com>
2025-01-14 17:47:50 -0600

    btn productos

--- src/app/page.js
+++ src/app/page.js
@@ -203,7 +203,7 @@ const Page = async () => {
               </div>
               {/*Controles*/}
               <div className="col ">
-                <ul className="product_tabnav_4 nav" role="tablist">
+                <ul className="product_tabnav_4 nav ul_li_center" role="tablist">
                   {/*Active*/}
                   <li>
                     <button
