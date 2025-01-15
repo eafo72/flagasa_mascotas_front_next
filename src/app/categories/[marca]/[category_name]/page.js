84e15ab0ba081948cc819ef2015af62bb19c6b09
alex <eafo72@gmail.com>
2025-01-15 14:57:34 -0600

    ajuste menu categorias

--- src/app/categories/[marca]/[category_name]/page.js
+++ src/app/categories/[marca]/[category_name]/page.js
@@ -91,7 +91,7 @@ const AllCategories = async ({ params }) => {
             <div className="filter_input_group">
               <form action="#">
                 <ul className="items_col ul_li">
-                  <li style={{display:"flex"}}>
+                  <li class="categories_control">
                     <h4 className="item_title text-uppercase">Fórmulas especializadas</h4>
                     <div className="select_option clearfix">
                       <CategorySelector
