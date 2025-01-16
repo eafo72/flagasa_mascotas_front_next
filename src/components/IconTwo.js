fe5fa546588495f91657c4a4edf0751f6a29f35c
alex <eafo72@gmail.com>
2025-01-15 20:14:36 -0600

    animate iconos

--- src/components/IconTwo.js
+++ src/components/IconTwo.js
@@ -10,7 +10,8 @@ export const IconTwo = () => {
         initial={{ opacity: 0, x: -100 }} // Inicia invisible y desplazado a la izquierda
         whileInView={{ opacity: 1, x: 0 }} // Se anima a la posición original con opacidad completa
         transition={{ duration: 0.8 }} // Duración de la animación
-        viewport={{ once: false, amount: 0.5 }} // Activa la animación cada vez que el 50% del elemento es visible
+        //viewport={{ once: false, amount: 0.5 }} // Activa la animación cada vez que el 50% del elemento es visible
+        viewport={{ once: true }} // Activa la animación cada vez que el 50% del elemento es visible
       >
         <img
           src="https://agencianuba.com/flagasamascotas_front_images/icons/icon-especialistas.png"
