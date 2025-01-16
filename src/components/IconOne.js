6fab29f9a2dea1f12a26a3ba4b4526f024934682
alex <eafo72@gmail.com>
2025-01-15 19:57:47 -0600

    animate iconos

--- src/components/IconOne.js
+++ src/components/IconOne.js
@@ -7,10 +7,10 @@ export const IconOne = () => {
     <>
       <motion.div
         className="icon"
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-        viewport={{ once: true }}
+        initial={{ opacity: 0, y: -100 }} // Inicia invisible y desplazado a la izquierda
+        whileInView={{ opacity: 1, y: 0 }} // Se anima a la posición original con opacidad completa
+        transition={{ duration: 0.8 }} // Duración de la animación
+        viewport={{ once: false, amount: 0.5 }} // Activa la animación cada vez que el 50% del elemento es visible
       >
         <img
           src="https://agencianuba.com/flagasamascotas_front_images/icons/icon-insumos.png"
