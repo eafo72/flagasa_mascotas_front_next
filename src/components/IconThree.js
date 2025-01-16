4428b14a288548922ab0646d55581ca2060af3f1
alex <eafo72@gmail.com>
2025-01-16 10:31:24 -0600

    animate roll

--- src/components/IconThree.js
+++ src/components/IconThree.js
@@ -10,9 +10,10 @@ export const IconThree = () => {
         //initial={{ opacity: 0, y: -100 }} // Inicia invisible y desplazado a la izquierda
         //whileInView={{ opacity: 1, y: 0 }} // Se anima a la posición original con opacidad completa
         //transition={{ duration: 0.8 }} // Duración de la animación
+        //transition={{ duration: 0.8, ease: "easeOut" }} // Duración de la animación con efecto suave
         initial={{ opacity: 0, rotate: -180 }} // Comienza invisible y rotado -180°
         whileInView={{ opacity: 1, rotate: 0 }} // Se vuelve visible y rota a 0°
-        transition={{ duration: 0.8, ease: "easeOut" }} // Duración de la animación con efecto suave
+        transition={{ type: "spring", stiffness: 100 }}
         viewport={{ once: false, amount: 0.5 }} // Activa la animación cada vez que el 50% del elemento es visible
       >
         <img
--- src/components/IconTwo.js
+++ src/components/IconTwo.js
@@ -11,8 +11,10 @@ export const IconTwo = () => {
         //whileInView={{ opacity: 1, y: 0 }} // Se anima a la posición original con opacidad completa
         //transition={{ duration: 0.8 }} // Duración de la animación
         //transition={{ duration: 0.8, ease: "easeOut" }} // Duración de la animación con efecto suave
-        initial={{ opacity: 0, rotate: -180 }} // Comienza invisible y rotado -180°
-        whileInView={{ opacity: 1, rotate: 0 }} // Se vuelve visible y rota a 0°
+        //initial={{ opacity: 0, rotate: -180 }} // Comienza invisible y rotado -180°
+        //whileInView={{ opacity: 1, rotate: 0 }} // Se vuelve visible y rota a 0°
+        initial={{ opacity: 0, rotateX: -90 }} // Empieza invisible y rotado sobre el eje X
+        whileInView={{ opacity: 1, rotateX: 0 }} // Se vuelve visible y regresa a posición normal
         transition={{ type: "spring", stiffness: 100 }}
         viewport={{ once: false, amount: 0.5 }} // Activa la animación cada vez que el 50% del elemento es visible
       >
