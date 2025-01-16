fecd20fd91b471a771aa0f3b3993c203953ce8e5
alex <eafo72@gmail.com>
2025-01-16 10:44:20 -0600

    descripcion

--- src/components/productPreviewCard.js
+++ src/components/productPreviewCard.js
@@ -411,7 +411,7 @@ export const ProductPreviewCard = ({ item }) => {
                           <span className="sale_price">$ {item.precio}</span>
                           */}
                         </div>
-                        <p className="mb-0">{item.descripcion}</p>
+                        <p className="mb-0" dangerouslySetInnerHTML={{ __html: item.descripcion }}></p>
                         <hr />
 
                         <a
