7cd597fb68d73f151dce63bde433bcdca0098f8a
alex <eafo72@gmail.com>
2025-01-15 20:18:32 -0600

    animate iconos

--- src/components/IconTwo.js
+++ src/components/IconTwo.js
@@ -7,8 +7,8 @@ export const IconTwo = () => {
     <>
       <motion.div
         className="icon"
-        initial={{ opacity: 0, x: -100 }} // Inicia invisible y desplazado a la izquierda
-        whileInView={{ opacity: 1, x: 0 }} // Se anima a la posición original con opacidad completa
+        initial={{ opacity: 0, y: -100 }} // Inicia invisible y desplazado a la izquierda
+        whileInView={{ opacity: 1, y: 0 }} // Se anima a la posición original con opacidad completa
         transition={{ duration: 0.8 }} // Duración de la animación
         //viewport={{ once: false, amount: 0.5 }} // Activa la animación cada vez que el 50% del elemento es visible
         viewport={{ once: true }} // Activa la animación cada vez que el 50% del elemento es visible
