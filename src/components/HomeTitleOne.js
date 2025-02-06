c4b73c78c137f3acc07b81ff0cd0e2c61723f97d
alex <eafo72@gmail.com>
2025-02-06 17:58:59 -0600

    home title one

--- src/components/HomeTitleOne.js
+++ src/components/HomeTitleOne.js
@@ -7,12 +7,14 @@ export const HomeTitleOne = () => {
     <>
       <motion.div
        initial={{ scale: 0.3, opacity: 0, y: -50 }} // Empieza pequeño, invisible y arriba
-       animate={{ scale: 1, opacity: 1, y: 0 }} // Termina en su tamaño normal y posición original
+       whileInView={{ scale: 1, opacity: 1, y: 0 }} // Se agranda y baja cuando entra en pantalla
        transition={{
          type: "spring", // Usa animación tipo resorte para el rebote
          stiffness: 300, // Controla la fuerza del resorte
          damping: 10, // Controla la cantidad de rebote
        }}
+       viewport={{ once: false, amount: 0.5 }} // Se ejecuta cada vez que entra al 50% en pantalla
+     
       >
        
        <h2 className="small_title">Fórmulas</h2>
