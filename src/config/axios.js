baf953300bb1bc82ed873f92b7e263c4c5b28a59
alex <eafo72@gmail.com>
2025-02-06 11:57:47 -0600

    api non-cache

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,12 @@
 import axios from "axios"
 
 const clienteAxios = axios.create({
- baseURL: "https://flagasa-mascotas-back.onrender.com"
- //baseURL: "http://localhost:4000"
+ baseURL: "https://flagasa-mascotas-back.onrender.com",
+ headers: {
+    "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+    "Pragma": "no-cache",
+    "Expires": "0"
+  }
 })
 
 export default clienteAxios
