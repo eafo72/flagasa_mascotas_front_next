987c73835df9a23a159d26897f7a24b8207cba3a
alex <eafo72@gmail.com>
2025-02-12 10:26:17 -0600

    fetch

--- src/app/page.js
+++ src/app/page.js
@@ -24,8 +24,9 @@ import { HomeTitleFour } from "../components/HomeTitleFour";
 
 const getCarrusel = async () => {
   try {
-    const res = await clienteAxios.get(`/carrusel/obtener`);
+    //const res = await clienteAxios.get(`/carrusel/obtener`);
     //console.log(res.data.carrusel)
+    const res = await fetch('https://flagasa-mascotas-back.onrender.com/carrusel/obtener', { cache: 'no-store' });
     return res.data.carrusel;
   } catch (error) {
     console.log(error);
