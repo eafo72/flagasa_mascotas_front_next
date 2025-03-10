b815d5f71bf607ce8ab1fd2ab1127776bb79affd
alex <eafo72@gmail.com>
2025-03-10 14:50:10 -0600

    borde del btn carrusel

--- src/app/page.js
+++ src/app/page.js
@@ -115,7 +115,7 @@ const Page = async () => {
                             </h4>
                             <Link
                               className="btn btn_rounded btn-view"
-                              style={{border:"none"}}
+                              style={{border:"2px solid transparent"}}
                               href={iteM.enlace}
                               data-animation="fadeInUp2"
                               data-delay=".7s"
